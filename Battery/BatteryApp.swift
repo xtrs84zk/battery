7e51e18a77140b9b8aa3a24dff9ff3419888bc7c
Javier Sanchez <14208092+xtrs84zk@users.noreply.github.com>
2026-03-24 16:08:42 -0600

    Initial Commit

---
+++ Battery/BatteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryApp.swift
+//  Battery
+//
+//  Created by Javier Sanchez on 24/03/26.
+//
+
+import SwiftUI
+
+@main
+struct BatteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
